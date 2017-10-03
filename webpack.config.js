1c98e611565e4bb7d4c05dba963b9bd55cc71d12
jenniferneale <jeneale6@gmail.com>
2017-10-03 15:14:57 -0400

    Added source map for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    devtool: 'inline-source-map',
     entry: './src/index.js', // the starting point for our program
     output: {
         path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
